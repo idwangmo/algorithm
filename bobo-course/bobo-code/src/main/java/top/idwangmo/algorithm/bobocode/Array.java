e3f38fbe081270fc238937b3ed373a35293e9e85
jinpeng.wang <#SFRnU&32Xj4tDXm7n>
2021-03-27 00:38:32 +0800

    feat(bobo): init

---
+++ bobo-course/bobo-code/src/main/java/top/idwangmo/algorithm/bobocode/Array.java
@@ -0,0 +1,14 @@
+package top.idwangmo.algorithm.bobocode;
+
+/**
+ * 数据的实现.
+ *
+ * @author idwangmo
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+
+    }
+}
